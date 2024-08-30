099e1777ca64fceb3f47aca820cc8f86e3cf2de4
Somil Sharma <somil81@icloud.com>
2024-08-30 20:58:11 +0530

    chore: code refactoring

--- server/index.js
+++ server/index.js
@@ -2,14 +2,9 @@ const express = require('express');
 const mongoose = require('mongoose');
 
 const app = express();
-
-const dburl = "mongodb+srv://iamsomilsharma:2AOywmUhZswLg5vM@cluster0.25hbmqm.mongodb.net/Texxeschool?retryWrites=true&w=majority&appName=Cluster0";
 app.use(express.json());
 
-mongoose.connect(dburl).then((function () {
-  console.log("connected to db");
-})).catch(err => console.log(err));
+const dburl = "mongodb+srv://iamsomilsharma:2AOywmUhZswLg5vM@cluster0.25hbmqm.mongodb.net/Texxeschool?retryWrites=true&w=majority&appName=Cluster0";
 
-app.listen(8081, () => {
-  console.log("server is connected");
-}); 
+mongoose.connect(dburl);
+app.listen(8081);
