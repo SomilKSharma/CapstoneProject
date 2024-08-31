1fe17b010f3a813aca9bc928db464f3a2ed5e8c1
Somil Sharma <somil81@icloud.com>
2024-08-31 09:31:31 +0530

    chore: add bcrypt

--- server/index.js
+++ server/index.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const userRoutes = require('./routes/userRoutes');
 
 const app = express();
 app.use(express.json());
 
 const dburl = "mongodb+srv://iamsomilsharma:2AOywmUhZswLg5vM@cluster0.25hbmqm.mongodb.net/BookMyShow?retryWrites=true&w=majority&appName=Cluster0";
+
+app.use('/api/users', userRoutes);
 
 mongoose.connect(dburl);
 app.listen(8081);
