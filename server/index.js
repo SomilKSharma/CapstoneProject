c4c441937d4668e33139b08cf7e109e667228718
Somil Sharma <somil81@icloud.com>
2024-08-31 08:06:33 +0530

    chore: add user router

--- server/index.js
+++ server/index.js
@@ -4,7 +4,7 @@ const mongoose = require('mongoose');
 const app = express();
 app.use(express.json());
 
-const dburl = "mongodb+srv://iamsomilsharma:2AOywmUhZswLg5vM@cluster0.25hbmqm.mongodb.net/Texxeschool?retryWrites=true&w=majority&appName=Cluster0";
+const dburl = "mongodb+srv://iamsomilsharma:2AOywmUhZswLg5vM@cluster0.25hbmqm.mongodb.net/BookMyShow?retryWrites=true&w=majority&appName=Cluster0";
 
 mongoose.connect(dburl);
 app.listen(8081);
